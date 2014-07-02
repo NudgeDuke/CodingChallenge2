98d06fd07a2ccf8e44e463fbaf54593398d3580e
Poppy Duke <Poppy.Duke@softwire.com>
2014-07-02 15:58:54 +0100

    Refactored for readability

--- CodingChallenge2/Program.cs
+++ CodingChallenge2/Program.cs
@@ -11,23 +11,19 @@ namespace CodingChallenge2
   class Program
   {
 
-    public static Dictionary<string, int> dictionary;
-    public static int frequency;
-
     static void Main(string[] args)
     {
-      dictionary = new Dictionary<string, int>();
 
       System.Console.WriteLine("TLS counter" +
                                "Enter frequency to find:");
-      frequency = int.Parse(Console.ReadLine());
+      var frequency = int.Parse(Console.ReadLine());
+      var text = "";
 
       try
       {
-        using (StreamReader sr = new StreamReader(@"E:\Challenges\tls.txt"))
+        using (var sr = new StreamReader(@"E:\Challenges\tls.txt"))
         {
-          String text = sr.ReadToEnd();
-          CountTls(text);
+          text = sr.ReadToEnd();
         }
       }
       catch (Exception e)
@@ -35,39 +31,12 @@ namespace CodingChallenge2
         System.Console.WriteLine("The file could not be read.");
       }
 
+      var counter = new Counter(frequency, text);
+      counter.CountTls();
+
       // Keep console window open in debug mode 
       System.Console.WriteLine("Any key to exit.");
       System.Console.ReadKey();
-    }
-
-    public static void CountTls(String text)
-    {
-      String tlsPattern = "(?=[a-z]{3})";
-      Regex regex = new Regex(tlsPattern, RegexOptions.IgnoreCase);
-
-      Match m = regex.Match(text);
-      while (m.Success)
-      {
-        String tls = text.Substring(m.Index,3);
-        if (dictionary.ContainsKey(tls))
-        {
-          dictionary[tls] = dictionary[tls] + 1;
-        }
-        else
-        {
-          dictionary.Add(tls, 1);
-        }
-        m = m.NextMatch();
-      }
-
-      foreach (KeyValuePair<string, int> entry in dictionary)
-      {
-        if (entry.Value == frequency)
-        {
-          System.Console.WriteLine(entry.Key + " " + frequency + " times");
-        }
-      }
-
     }
   }
 }
