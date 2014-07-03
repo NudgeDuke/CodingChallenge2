a97965e4fa2815577934e4b479dac241b074a622
Poppy Duke <Poppy.Duke@softwire.com>
2014-07-03 11:38:22 +0100

    Alternative one line solution

---
+++ CodingChallenge2/OneLineSolution.cs
@@ -0,0 +1,18 @@
+﻿namespace CodingChallenge2
+{
+  using System;
+  using System.Linq;
+  using System.Text.RegularExpressions;
+
+  internal class OneLineSolution
+  {
+    public void SearchTlsFrequency(String text, int frequency)
+    {
+      var inOneLine = Regex.Matches(text, @"(?=[a-z]{3})", RegexOptions.IgnoreCase).Cast<Match>().GroupBy(m => text.Substring(m.Index, 3).ToLower()).Where(g => g.Count() == frequency).Select(tls => tls.Key);
+      foreach (var tls in inOneLine)
+      {
+        Console.WriteLine(tls + " " + frequency + " times");
+      }
+    }
+  }
+}
--- CodingChallenge2/Program.cs
+++ CodingChallenge2/Program.cs
@@ -7,6 +7,8 @@
   {
     private static void Main(string[] args)
     {
+      var oneLine = new OneLineSolution();
+
       Console.WriteLine("TLS counter\n" +
                         "Enter frequency to find:");
       var frequency = int.Parse(Console.ReadLine());
@@ -24,8 +26,12 @@
         Console.WriteLine("The file could not be read.");
       }
 
-      var counter = new Counter(frequency, text);
-      counter.CountTls();
+      // Original solution
+      // var counter = new Counter(frequency, text);
+      // counter.CountTls();
+
+      // One line of LINQ
+      oneLine.SearchTlsFrequency(text, frequency);
 
       // Keep console window open in debug mode
       Console.WriteLine("Any key to exit.");
